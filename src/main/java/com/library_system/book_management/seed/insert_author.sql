8fdb1b78396958b5bbb5a42628e3fcaef8758522
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-10-14 16:10:44 -0300

    test: add an author to the table

---
+++ src/main/java/com/library_system/book_management/seed/insert_author.sql
@@ -0,0 +1,3 @@
+INSERT INTO author(full_name, abbreviation_name, birthday) VALUES('Paul Joseph Deitel', 'Deitel, Paul J.', '1965-12-14')
+
+INSERT INTO author(full_name, abbreviation_name, birthday) VALUES('Harvey Morris Deitel', 'Deitel, Harvey M.', '1945-10-14')
