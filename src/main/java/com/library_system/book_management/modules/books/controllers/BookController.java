a8b4858a845cd9dfe45e1cd87b130cd383acfe69
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-10-13 22:11:46 -0300

    feat: create endpoints to get all books and get a book by ID

---
+++ src/main/java/com/library_system/book_management/modules/books/controllers/BookController.java
@@ -0,0 +1,30 @@
+package com.library_system.book_management.modules.books.controllers;
+
+import com.library_system.book_management.modules.books.dtos.RecoveryBookDto;
+import com.library_system.book_management.modules.books.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    BookService bookService;
+
+    @GetMapping
+    public ResponseEntity<List<RecoveryBookDto>> getBooks() {
+        return ResponseEntity.ok(bookService.getBooks());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<RecoveryBookDto>> getBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+}
